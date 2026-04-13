048ca5386199aedfba432367b42edd8cd7836d65
Gaven <ghuynh@student.cscc.edu>
2026-04-12 22:46:07 -0400

    Move bullet import from type checking into main file

--- arsenal.py
+++ arsenal.py
@@ -1,9 +1,10 @@
 import pygame
+from bullet import Bullet
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
-    from bullet import Bullet
+    
 
 class Arsenal:
     def __init__(self, game: "AlienInvasion") -> None:
