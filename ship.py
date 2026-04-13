4afec55a5ac11ae85a970600d6eb67195410d643
Gaven <ghuynh@student.cscc.edu>
2026-04-12 20:14:56 -0400

    Get rect for screen, create rect around image, and postion ship at bottom center of screen

--- ship.py
+++ ship.py
@@ -8,8 +8,12 @@ class Ship:
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
+        self.screen_rect = self.screen.get_rect()
         
         self.image= pygame.image.load(self.settings.ship_file)
         self.image= pygame.transform.scale(self.image,
             (self.settings.ship_w, self.settings.ship_h)
              )
+        
+        self.rect = self.image.get_rect()
+        self.rect.midbottom = self.screen_rect.midbottom
