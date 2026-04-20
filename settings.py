05d8ae9850b43c743a56cda8bf9ead96eec6841d
Gaven <ghuynh@student.cscc.edu>
2026-04-20 00:18:14 -0400

    Copy changes to settings from participation activity

--- settings.py
+++ settings.py
@@ -29,10 +29,19 @@ class Settings:
         self.ship_w = 40
         self.ship_h = 60
         self.ship_speed = 10
+        self.starting_ship_count = 3
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
+        self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
         self.bullet_speed = 15
         self.bullet_w = 25
         self.bullet_h = 80
         self.bullet_amount = 5
+        
+        self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
+        self.alien_w = 40
+        self.alien_h = 40
+        self.fleet_speed = 5
+        self.fleet_direction = 1
+        self.fleet_drop_speed = 40
