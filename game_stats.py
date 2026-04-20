4640cc603743089af3c05df7007a88fff5715d35
Gaven <ghuynh@student.cscc.edu>
2026-04-20 00:09:42 -0400

    Copy GameStats class from participation activity

--- game_stats.py
+++ game_stats.py
@@ -5,5 +5,8 @@ Purpose: Contains the volitile data of the game.
 Starter Code: Built upon starter code and assets provided by the class
 then fully fleshed out by me in a different repo. That was imported
 into this repo for further editing to make it more my own.
-Date: April 13th 2026
+Date: April 20th 2026
 """
+class GameStats():
+    def __init__(self, ships_left) -> None:
+        self.ships_left = ships_left
