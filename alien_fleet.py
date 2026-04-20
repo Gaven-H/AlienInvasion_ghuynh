69ffabc45306ec8c334c45be68cc4290c3503c2b
Gaven <ghuynh@student.cscc.edu>
2026-04-20 00:03:17 -0400

    Docstring for alien_fleets

---
+++ alien_fleet.py
@@ -0,0 +1,9 @@
+"""
+Program: Alien Invasion - 
+Author: Gaven Huynh
+Purpose: Manages updating, spawning and collision of alien fleets.
+Starter Code: Built upon starter code and assets provided by the class
+then fully fleshed out by me in a different repo. That was imported
+into this repo for further editing to make it more my own.
+Date: April 20th 2026
+"""
