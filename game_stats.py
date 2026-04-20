9352de6e74ce5ddf3d18433fe44aa21ae8d8e247
Gaven <ghuynh@student.cscc.edu>
2026-04-20 00:01:20 -0400

    Docstring for game_stats.py

---
+++ game_stats.py
@@ -0,0 +1,9 @@
+"""
+Program: Alien Invasion - Game Stats
+Author: Gaven Huynh
+Purpose: Contains the volitile data of the game.
+Starter Code: Built upon starter code and assets provided by the class
+then fully fleshed out by me in a different repo. That was imported
+into this repo for further editing to make it more my own.
+Date: April 13th 2026
+"""
