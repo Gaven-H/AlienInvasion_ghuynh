cb84dffa15ceae3b39bbad5efca4370b197d1951
Gaven <ghuynh@student.cscc.edu>
2026-04-20 00:05:36 -0400

    Docstring for alien.py

---
+++ alien.py
@@ -0,0 +1,9 @@
+"""
+Program: Alien Invasion - Alien
+Author: Gaven Huynh
+Purpose: Represents the individual enemy aliens that move towards the ship.
+Starter Code: Built upon starter code and assets provided by the class
+then fully fleshed out by me in a different repo. That was imported
+into this repo for further editing to make it more my own.
+Date: April 20th 2026
+"""
